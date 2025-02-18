4ed7a3fde54fdc9ff1b56aa3d1836b3242a54775
AlexandReznik <saha.reznik2004@gmail.com>
2025-02-18 15:08:21 +0100

    add models and basic structure of project

---
+++ app/products/model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.common.database import Base
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    quantity = Column(Integer)
+    receipt_id = Column(Integer, ForeignKey('receipts.id'))
+    receipt = relationship('Receipt', back_populates='products')
+    total = Column(Float)
---
+++ app/users/model.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.common.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    login = Column(String, unique=True)
+    password = Column(String)
+    receipts = relationship('Receipt', back_populates='user')
